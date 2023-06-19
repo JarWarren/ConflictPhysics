8cdf5278a8df9c30c4cdf97c0cbd598aef914567
JarWarren <wrrn24@gmail.com>
2023-06-18 21:06:07 -0600

    raylib for visual testing

--- tests/main.c
+++ tests/main.c
@@ -1,38 +1,72 @@
 #include "../conflict_physics.h"
+#include <raylib.h>
+
 #include <stdio.h>
 
-#define COUNT 2
+#define COUNT 3
+
+// TODO: visual test with raylib
 
 int main(void) {
-
     CflBody bodies[COUNT];
-    bodies[0] = cflCircle(100, 100, 20);
+
+    // player body
+    bodies[0] = cflCircle(100, 100, 10);
+    bodies[0].isStatic = false;
     bodies[0].categoryMask = 1;
-    bodies[0].collisionMask = 1;
+    bodies[0].collisionMask = 2;
+    bodies[0].detectionMask = 2;
+    bodies[0].onTrigger = NULL;
 
-    bodies[1] = cflCircle(110, 100, 20);
-    bodies[1].categoryMask = 1;
+
+    // wall body
+    bodies[1] = cflRectangle(200, 200, 100, 100);
+    bodies[1].isStatic = true;
+    bodies[1].categoryMask = 2;
     bodies[1].collisionMask = 1;
+    bodies[1].detectionMask = 1;
+    bodies[1].onTrigger = NULL;
 
-//    bodies[2] = cflRectangle(100, 110, 35, 40);
-//    bodies[2].categoryMask = 1;
-//    bodies[2].collisionMask = 1;
-//
-//    bodies[3] = cflRectangle(90, 100, 45, 50);
-//    bodies[3].categoryMask = 1;
-//    bodies[3].collisionMask = 1;
+    // floor body
+    bodies[2] = cflRectangle(0, 400, 800, 50);
+    bodies[2].isStatic = true;
+    bodies[2].categoryMask = 2;
+    bodies[2].collisionMask = 1;
+    bodies[2].detectionMask = 1;
+    bodies[2].onTrigger = NULL;
 
-    // print the positions of all the bodies
-    printf("initial positions:\n");
-    int i;
-    for (i = 0; i < COUNT; i++) {
-        printf("body %d: (%f, %f)\n", i, bodies[i].x, bodies[i].y);
+
+    InitWindow(800, 450, "conflict_physics test");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        if (IsKeyDown(KEY_LEFT)) {
+            bodies[0].x -= 1;
+        }
+        if (IsKeyDown(KEY_RIGHT)) {
+            bodies[0].x += 1;
+        }
+        if (IsKeyDown(KEY_UP)) {
+            bodies[0].y -= 1;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            bodies[0].y += 1;
+        }
+
+        cflSolve(bodies, COUNT);
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        DrawCircle(bodies[0].x, bodies[0].y, bodies[0].collider.value.circle.radius, SKYBLUE);
+        for (int i = 1; i < COUNT; i++) {
+            if (bodies[i].collider.type == CFL_COLLIDER_TYPE_CIRCLE) {
+                DrawCircle(bodies[i].x, bodies[i].y, bodies[i].collider.value.circle.radius, RED);
+            } else if (bodies[i].collider.type == CFL_COLLIDER_TYPE_RECTANGLE) {
+                DrawRectangle(bodies[i].x, bodies[i].y, bodies[i].collider.value.rectangle.width, bodies[i].collider.value.rectangle.height, RED);
+            }
+        }
+        EndDrawing();
     }
 
-    cflSolve(bodies, COUNT);
-
-    printf("\nfinal positions:\n");
-    for (i = 0; i < COUNT; i++) {
-        printf("body %d: (%f, %f)\n", i, bodies[i].x, bodies[i].y);
-    }
+    CloseWindow();
 }
